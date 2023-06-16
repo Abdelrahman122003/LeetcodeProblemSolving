9cd129e182235e768fc3220f97684dfc56189c39
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-06-16 10:12:24 +0300

    Binary Tree Postorder Traversal

---
+++ Easy/problem145.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+     vector<int>v;
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        re(root);
+        return (v);
+    }
+    void re(TreeNode* root){
+        if(!root){
+            return;
+        }
+        re(root->left);
+        re(root->right);
+        v.push_back(root->val);
+        // root->left->val;
+        // root->val;
+        // root->right->right;
+    }
+};
