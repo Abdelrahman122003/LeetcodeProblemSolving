674af82fd980fdf274e53e6d439ff18bd1ffe471
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-06-16 09:58:11 +0300

    Running Sum of 1d Array

---
+++ Easy/problem1480.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>pre(n);
+        pre[0] = nums[0];
+        int index = 1;
+        for (int i = 1; i < n; ++i)pre[i] = pre[i-1] + nums[i];
+        return pre;
+    }
+};
