4a41c298e658566be1b8a93ad457e4efee7553c0
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-06-16 19:08:43 +0300

    Remove Duplicates from Sorted Array II

---
+++ Meduim/problem80.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        map<int, int>m;
+        int j = 0;
+        for(int i = 0; i < nums.size();i++){
+          if(m[nums[i]] < 2){
+            m[nums[i]]++;
+            nums[j++] = nums[i];
+          }
+        }
+        return j;
+    }
+};
