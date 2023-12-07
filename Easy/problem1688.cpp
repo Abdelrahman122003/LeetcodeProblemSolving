1879987c565d3d44b18501f2896515a60ec25160
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-12-07 17:30:27 +0200

    Count of Matches in Tournament

---
+++ Easy/problem1688.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+    int ans = 0;
+    while(n > 1){
+        ans += (n%2)?((n-1)/2):n/2;
+        n = (n%2)?((n - 1) / 2 + 1):n/2;
+    }
+    return ans;  
+    }
+};
