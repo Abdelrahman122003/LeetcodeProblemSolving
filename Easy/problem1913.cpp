02ced9ed90ab7c8ad4052abfdcf9f531673ed2f0
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-01-02 09:55:28 +0200

    Maximum Product Difference Between Two Pairs

---
+++ Easy/problem1913.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size()-1;
+        int mpd=(nums[n]*nums[n-1])-(nums[0]*nums[1]) ; 
+        return mpd;
+    }
+};
