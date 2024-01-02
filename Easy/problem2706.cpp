e57f408473258eb8588ee804115d81ec6e7dd554
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-01-02 09:54:12 +0200

    Buy Two Chocolates

---
+++ Easy/problem2706.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int ans = 0;
+        ans = (money - (prices[0]+prices[1]) >= 0)?money - (prices[0]+prices[1]):money;
+
+        return ans;
+    }
+};
