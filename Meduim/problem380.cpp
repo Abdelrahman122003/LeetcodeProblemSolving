646b80115cb994855aaa09c8459c9ee247998230
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-01-16 09:51:50 +0200

    Insert Delete GetRandom O(1)

---
+++ Meduim/problem380.cpp
@@ -0,0 +1,39 @@
+class RandomizedSet {
+public:
+        unordered_set<int>s;
+    RandomizedSet() {
+    }
+    
+    bool insert(int val) {
+        if(!s.count(val))
+        {
+                s.insert(val);
+                return true;
+        }
+        return false;
+    }
+    
+    bool remove(int val) {
+        if(s.count(val))
+        {
+                s.erase(val);
+                return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+          int random = rand()%s.size();
+            auto it = s.begin();
+            advance(it, random);
+          return *it;
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
