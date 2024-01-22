4bf2bfd2394a63be0afdbb9c9e61cea35efca024
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-01-22 09:56:02 +0200

    House Robber

--- Meduim/problem198.cpp
+++ Meduim/problem198.cpp
@@ -1,3 +1,4 @@
+// first solution dp(up to bottom)
 class Solution {
 public:
         
@@ -13,5 +14,17 @@ public:
              memset(dp, -1, sizeof dp);
            return solve(0, nums);
              
+    }
+};
+// second solution dp(bottom to up) 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+            if(nums.size()==1)return(nums[0]);
+            nums[1] = max(nums[0], nums[1]);
+            for(int i = 2;i < nums.size();i++){
+                    nums[i] = max(nums[i] + nums[i-2], nums[i-1]);
+            }
+            return nums[nums.size()-1];
     }
 };
