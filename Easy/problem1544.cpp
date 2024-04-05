863835a2f5e30435692497e9c3cd044df33177f3
3bdelr7man <abdelrhmansayed122003@gmail.com>
2024-04-05 14:36:01 +0200

    Make The String Great

--- Easy/problem1544.cpp
+++ Easy/problem1544.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     string makeGood(string s) {
        int n = s.size();
-        deque<char>d;
-        for (int i = 0; i < n; i++) {
-            if(i == 0) {
-                d.push_back(s[i]);
-                continue;
-            }
-            if(((char)(d.back() + 32) == s[i] || d.back() == (char)(s[i] + 32)) && !d.empty()){
-                d.pop_back();
-                continue;
-            }
-            else
-            {
-                d.push_back(s[i]);
-            }
+            string str = "";
+    for (int i = 0; i < n; i++) {
+        if(!str.empty() && (abs(str.back() - s[i]) == 32) && str.back() != s[i]){
+                str.pop_back();
+        }else{
+            str += s[i];
         }
-        string str = "";
-        // cout << d.size() << endl;
-        if(!d.empty()){
-            for (int i = 0; i < d.size(); i++) str += d[i];
+    }
             return str;
-        }
-        return "";
     }
 };
