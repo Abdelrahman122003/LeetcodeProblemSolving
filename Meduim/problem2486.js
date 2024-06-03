be8e20b248bb446143370d1ad7eb6d10f7963bc8
3bdelr7man <abdelrhmansayed122003@gmail.com>
2024-06-03 09:51:13 +0300

    Append Characters to String to Make Subsequence

---
+++ Meduim/problem2486.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharacters = function (s, t) {
+  let i = 0,
+    j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) j++;
+    i++;
+  }
+  return t.length - j;
+};
