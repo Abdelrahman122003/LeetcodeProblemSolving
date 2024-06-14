0f409d71699df9984342c61f36bb454933e8cf64
3bdelr7man <abdelrhmansayed122003@gmail.com>
2024-06-14 16:32:38 +0300

    Minimum Number of Moves to Seat Everyone

---
+++ Easy/problem2037.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+            int counter = 0;
+            for(int i = 0 ; i< seats.size();i++)
+                    counter += abs(seats[i] - students[i]);
+            return counter;
+    }
+};
