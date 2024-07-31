c74e9757ab7efa533fee4478dfed216e22c8d8a5
3bdelr7man <abdelrhmansayed122003@gmail.com>
2024-07-31 15:58:56 +0300

    Sort Array by Increasing Frequency

--- Easy/problem1636.cpp
+++ Easy/problem1636.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    static bool compare(pair<int, int>a, pair<int, int>b){
-            if(a.first < b.first)
-                    return true;
-            if(a.first == b.first && a.second > b.second)
-                    return true;
-            return false;
-    }
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int, int>m;
+        unordered_map<int, int>freq;
             for(auto k : nums)
-                    m[k]++;
-            vector<pair<int, int>>freq;
-            for(auto [k, v] : m)
-                    freq.push_back({v, k});
-            sort(freq.begin(), freq.end(), compare);
-            int i = 0;
-            for(auto [v, k] : freq)
-                    while(v--)
-                            nums[i++] = k;
+                    freq[k]++;
+            // Customized Sorting
+            sort(nums.begin(), nums.end(), [&](int a, int b){
+                    if(freq[a] == freq[b]){
+                            return a > b;
+                    }
+                    return freq[a] < freq[b];
+            }
+                );
             return nums;
     }
 };
